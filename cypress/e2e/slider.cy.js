e65e55540053cf5a4318151ff50b26ad421398a5
Eve0028 <62481375+Eve0028@users.noreply.github.com>
2025-03-15 09:39:09 +0100

    add: Base project

---
+++ cypress/e2e/slider.cy.js
@@ -0,0 +1,31 @@
+describe('Swiper Gallery Test', function () {
+  it('Checks if second slide contains "United Kingdom"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if third slide contains "Paris"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if third slide contains "Paris"', function () {
+    cy.visit('http://localhost:3000');
+    // slide1 = cy.get('swiper-slide-active .card-description>h1')
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('swiper-slide-active .card-description>h1').should('not.have.value', 'Rome')
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('swiper-slide-active .card-description>h1').should('not.have.value', 'London')
+  });
+});
